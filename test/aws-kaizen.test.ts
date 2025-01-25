eae39aa3c72176fb131e8de7621f8d2949019bf5
romanpedchenko <r.pedchenko@mylette.nl>
2025-01-25 01:18:06 +0100

    first commit

---
+++ test/aws-kaizen.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as AwsKaizen from '../lib/aws-kaizen-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/aws-kaizen-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new AwsKaizen.AwsKaizenStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
